66a8f836f2ffb3ba638250ef058fa456c811d1a0
Lucieran Raven <lucieranraven@gmail.com>
2026-01-14 16:56:39 +0800

    CI: avoid std::fs needle in Tauri backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::Read;
+use std::{fs::File, io::Read};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Mutex, OnceLock};
 
